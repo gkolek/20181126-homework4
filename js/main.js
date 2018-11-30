3f5c08119ad5a8aa00e0d7f009c86e0cd989f16e
Grzegorz Kolek <gkolek@gazeta.pl>
2018-11-30 15:41:31 +0100

    done by addEventListener

--- js/main.js
+++ js/main.js
@@ -1,30 +1,49 @@
-(function () {
-  var clicks1 = 0;
-  function substractClick() {
-    clicks1 -= 1;
-    document.getElementById("clickCounter1").innerHTML = clicks1;
-    // console.log(clicks1);
-  }
-  function addClick() {
-    clicks1 += 1;
-    document.getElementById("clickCounter1").innerHTML = clicks1;
-    // console.log(clicks1);
-  }
-  window.substractClick = substractClick;
-  window.addClick = addClick;
-})();
+// (function () {
+//   var clicks1 = 0;
+//   function substractClick() {
+//     clicks1 -= 1;
+//     document.getElementById("clickCounter1").innerHTML = clicks1;
+//     // console.log(clicks1);
+//   }
+//   function addClick() {
+//     clicks1 += 1;
+//     document.getElementById("clickCounter1").innerHTML = clicks1;
+//     // console.log(clicks1);
+//   }
+//   window.substractClick = substractClick;
+//   window.addClick = addClick;
+// })();
 
 
 
-var clicks = 0;
+// var clicks = 0;
+// function minusClick() {
+//   clicks -= 1;
+//   document.getElementById("clickCounter").innerHTML = clicks;
+//   // console.log(clicks);
+// };
+// function plusClick() {
+//   clicks += 1;
+//   document.getElementById("clickCounter").innerHTML = clicks;
+//   // console.log(clicks);
+// };
+
+(function (){
+var add = document.querySelector('.add')
+var sub = document.querySelector('.substract')
+add.addEventListener('click', plusClick)
+sub.addEventListener('click', minusClick)
+
+var clicks1 = 0;
 function minusClick() {
-  clicks -= 1;
-  document.getElementById("clickCounter").innerHTML = clicks;
-  console.log(clicks);
+  clicks1 -= 1;
+  document.getElementById("clickCounter1").innerHTML = clicks1;
 };
 function plusClick() {
-  clicks += 1;
-  document.getElementById("clickCounter").innerHTML = clicks;
-  console.log(clicks);
+  clicks1 += 1;
+  document.getElementById("clickCounter1").innerHTML = clicks1;
 };
+
+})();
+
 
