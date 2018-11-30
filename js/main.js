cd3b0c1fc6dbf95c3f5e56ec8779f52cf1d7075b
Grzegorz Kolek <gkolek@gazeta.pl>
2018-11-30 13:32:48 +0100

    counter locked in local scope by self-executing anonymous function

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-
-var clicks = 0;
-function substractClick() {
+(function () {
+  var clicks = 0;
+  function substractClick() {
     clicks -= 1;
     document.getElementById("clickCounter1").innerHTML = clicks;
-console.log(clicks);
-};
-function addClick() {
+    console.log(clicks);
+  }
+  function addClick() {
     clicks += 1;
     document.getElementById("clickCounter1").innerHTML = clicks;
-};
+    console.log(clicks);
+  }
+  window.substractClick = substractClick;
+  window.addClick = addClick;
+})();
+
 
 
 var clicks1 = 0;
 function minusClick() {
-    clicks1 -= 1;
-    document.getElementById("clickCounter").innerHTML = clicks1;
+  clicks1 -= 1;
+  document.getElementById("clickCounter").innerHTML = clicks1;
 };
 function plusClick() {
-    clicks1 += 1;
-    document.getElementById("clickCounter").innerHTML = clicks1;
+  clicks1 += 1;
+  document.getElementById("clickCounter").innerHTML = clicks1;
 };
 
